760e9282f916e35a7801982ac5ca0c0b6c3ccab5
mohamed-12-4 <m12mm.4m2005@gmail.com>
2022-08-20 19:57:22 +0000

    linera

--- Python/LinearRegression.py
+++ Python/LinearRegression.py
@@ -1,48 +1,50 @@
 import numpy as np
-
+"""
 class LinearRegression:
-    def __init__(self, alpha=0.01, n_iter=100, w=0, b=0):
+    def __init__(self, alpha=0.01, n_iter=100, b=0):
         self.alpha = alpha
         self.n_iter = n_iter
-        self.w = w
+        self.w = None
         self.b = b
 
-    def fit(self, x, y):
-
+    def fit(self, X_train, y):
+        self.w = np.zeros(X_train.shape[1], dtype=np.float64)
         for i in range(self.n_iter):
-            dw, db = self.compute_gradient(self.w, self.b, x, y)
+            dw, db = self.compute_gradient(self.w, self.b, X_train, y)
             self.w -= self.alpha * dw
             self.b -= self.alpha * db
             if i % 10 == 0:
-                print(self.cost_function(x, y, self.w, self.b))
+                print(self.cost_function(X_train, y, self.w, self.b))
 
     def cost_function(self, x, y, w, b):
-        cost = 0
+        cost = 0.0
         m = x.shape[0]
         for i in range(x.shape[0]):
-            fw_b = w*x[i] + b
-            cost += (fw_b - y[i]) ** 2
+            fw_b = np.dot(x[i], w) + b
+            cost = cost + (fw_b - y[i])**2
 
-        return 1/2*m*(cost)
+        return cost / 2*m
 
     def compute_gradient(self, w, b, x, y):
-        dw, db = 0, 0
         m = x.shape[0]
-        for i in range(x.shape[0]):
-            fw_b = w*x[i] + b
-            dw_i = (fw_b - y[i])*x[i]
-            db_i = fw_b - y[i]
-            dw += dw_i
-            db += db_i
+        n = x.shape[1]
+        db = 0
+        dw = np.zeros((n,), dtype=np.float)
+        for i in range(m):
+            err = (np.dot(x[i], w) + b) - y[i]
+            for j in range(n):
+                dw[j] = dw[j] + err * x[i, j]
+            db += err
 
         return dw/m, db/m
 
     def predict(self,x):
-        return x*self.w + self.b
+        return np.dot(x, w) + self.b
+""" 
 
-x_train = np.array([1.0, 2.0, 3, 4, 5, 6])   #features
-y_train = np.array([300.0, 500.0, 700, 900, 1100, 1300])   #target value
+X_train = np.array([[2104, 5, 1, 45], [1416, 3, 2, 40], [852, 2, 1, 35]])
+y_train = np.array([460, 232, 178])
 
-model = LinearRegression(alpha=0.01, n_iter=10000)
-model.fit(x_train, y_train)
-print(f"Prediction: {model.predict(7)} ") 
+
+model = LinearRegression(alpha=0.01, n_iter=200)
+model.fit(X_train, y_train)
