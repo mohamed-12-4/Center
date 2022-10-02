c37826b98feb1710f6d1b1cc75acd55fa9d14fb7
mohamed-12-4 <m12mm.4m2005@gmail.com>
2022-10-02 08:42:45 +0000

    c++ project 1 !!

---
+++ C++/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <math.h>
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+// TODO
+
+bool isPowerOfTwo(int n)
+{
+    return (ceil(log2(n)) == floor(log2(n)));
+}
+int main(){
+    
+}
+
+//int check(A, B, C, D,){}
